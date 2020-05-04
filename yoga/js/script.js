f45fb3c0fac98bf44f2481037a9e9c468da10aa8
Roman <rvsh@bk.ru>
2020-05-04 16:45:09 +0300

    task_11

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -84,5 +84,31 @@ window.addEventListener('DOMContentLoaded', function() {
 
     setClock('timer', dedline);
 
+    // Modal
 
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close'),
+        inf = document.querySelector('.info'),
+        descriptionBtn = document.querySelectorAll('.description-btn');
+
+    more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
+    
+    inf.addEventListener('click', function(event) {
+        if (event.target && event.target.classList.contains('description-btn')) {
+            overlay.style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        }
+    });
+    
 });
